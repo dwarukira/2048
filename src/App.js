d1aae72627568171a130df1c94930b00f81cbc3b
davenjeru <dave.njeru@andela.com>
2019-02-11 15:56:24 +0300

    2048 gameplay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import Cell from './components/cell';
+import _ from 'lodash';
 
+// Using destructing
+const transpose = m => m.map((x,i) => m.map(x => x[i]));
 
 const deflatten = (arr) => {
   let deflattened = [];
@@ -12,11 +15,11 @@ const deflatten = (arr) => {
     if (((index + 1) % 4) === 0 ){
       deflattened.push(row);
       row = [];
-    }   
+    }
   }
 
   return deflattened;
-}
+};
 
 const random = (arr) => {
   const output = [...arr];
@@ -26,24 +29,55 @@ const random = (arr) => {
     ran2 = Math.floor(Math.random() *16);
   }
   output[ran1] = output[ran2] = 2;
-  
+
   return deflatten(output);
-}
+};
+
 const createGridArray = () => {
-  let grid = []
+  let grid = [];
   for (let i = 0; i < 4; i++) {
-    let col = []
+    let col = [];
     for (let j = 0; j < 4; j++) {
       col[j] = 0;
     }
     grid.push(col);
   }
   return random(grid.flat());
-}
+};
 
-const isDirectinalKey = (code) => {
-  return code >= 37 && code <= 40 
-}
+const isDirectionalKey = (code) => {
+  return code >= 37 && code <= 40
+};
+
+
+const merge = (row, indexComputation) => {
+  console.log(indexComputation, row);
+  let outputRow = [];
+  for (let j = 0; j < row.length; j++) {
+    if (row[j] === row[j+1]){
+      outputRow.push(row[j]+row[j+1]);
+      row.splice(j+1, 1);
+    }else{
+      outputRow.push(row[j]);
+    }
+  }
+  if (outputRow.length < 4){
+    const a = new Array((4 - outputRow.length)).fill(0);
+    switch (indexComputation) {
+      case 'left':
+        return outputRow.concat(a);
+      case 'right':
+        return a.concat(outputRow);
+      case 'up':
+        return outputRow.concat(a);
+      case 'down':
+        return a.concat(outputRow);
+      default:
+    }
+  }
+  return outputRow;
+};
+
 class App extends Component {
   // draw grid
   // add event listner
@@ -51,40 +85,95 @@ class App extends Component {
 
   componentDidMount(){
     window.addEventListener("keydown",(e) => {
-      if(isDirectinalKey(e.keyCode)) {
-        this.handleKeyPress(e) 
+      if(isDirectionalKey(e.keyCode)) {
+        this.handleKeyPress(e)
       }
     })
   }
 
-  
+
   handleKeyPress = (e) => {
-    console.log(e);
     switch(e.keyCode){
       case 37:
-        this.handleHorizontal();
+        this.handleHorizontal('left');
+        break;
+      case 39:
+        this.handleHorizontal('right');
+        break;
+      case 38:
+        this.handleVertical('up');
+        break;
+      case 40:
+        this.handleVertical('down');
         break;
       default:
     }
-  }
+  };
 
   state = {
     grid: createGridArray()
-  }
+  };
 
   createGrid = ()=> {
-    return this.state.grid.map((row, r) => <div key={`row-${r+1}`} className="grid-row">{ 
+    return this.state.grid.map((row, r) => <div key={`row-${r+1}`} className="grid-row">{
         row.map((cell, c) => <Cell key={`${r+1}-${c+1}`} value={cell}/>)}</div>)
-  }
-  
+  };
 
-  handleHorizontal = () => {
-    console.log('Left');
-  }
-  
+  handleMovement = (grid, indexComputation) => {
+    const newGrid = [];
+    let row = [];
+    for (let i = 0; i < grid.length; i++) {
+      row = [];
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] !== 0){
+          row.push(grid[i][j]);
+        }
+      }
+      newGrid.push(merge(row, indexComputation));
+    }
+
+    // TODO Check to remove flat()
+    if(_.isEqual(newGrid.flat(), grid.flat())){
+      return newGrid;
+    }
+
+    const flatGrid = newGrid.flat();
+    let ranIndex = Math.floor(Math.random() * flatGrid.length);
+
+    let count = 0;
+    while (flatGrid[ranIndex] !== 0) {
+      ranIndex = Math.floor(Math.random() * flatGrid.length);
+      count ++;
+      if (count >  16){
+        // TODO Game over on illegal move
+        return newGrid;
+      }
+    }
+
+    const ranFloat = Math.random();
+    if (ranFloat < 0.5){
+      flatGrid[ranIndex] = 2;
+    } else {
+      flatGrid[ranIndex] = 4;
+    }
+
+    return deflatten(flatGrid);
+  };
+
+
+  handleHorizontal = (indexComputation) => {
+    const { grid } = this.state;
+
+    this.setState({ grid: this.handleMovement(grid, indexComputation)});
+  };
+
+  handleVertical = (indexComputation) => {
+    const { grid } = this.state;
+    const newGrid = this.handleMovement(transpose(grid), indexComputation);
+    this.setState({ grid: transpose(newGrid)});
+  };
+
   render() {
-    // console.log(this.state);
-    
     return (
         <div className="container">
           <div className="heading">
--- src/components/cell.js
+++ src/components/cell.js
@@ -3,6 +3,7 @@ import React from "react"
 
 
 const Cell = ({ id, value }) => (
-    <div className="grid-cell" id={id}> { value != 0 ? value: ''   } </div>
-)
+    <div className="grid-cell" id={id}> { value != 0 ? (<div className={`tile tile-${value}`}>{value}</div>) : ''   } </div>
+);
+
 export default Cell;
