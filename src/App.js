ba021931fd8c315c7d49b5b52b3df81dcfefd9a6
davenjeru <dave.njeru@andela.com>
2019-02-11 11:11:08 +0300

    2048 boilerplate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <div className="container">
+          <div className="heading">
+            <h1 className="title">2048</h1>
+            <div className="score-container">0</div>
+          </div>
+          <p className="game-intro">Join the numbers and get to the <strong>2048 tile!</strong></p>
+
+          <div className="game-container">
+            <div className="game-message">
+              <p></p>
+              <div className="lower">
+                <a className="retry-button">Try again</a>
+              </div>
+            </div>
+
+            <div className="grid-container">
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+              </div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+              </div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+              </div>
+              <div className="grid-row">
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+                <div className="grid-cell"></div>
+              </div>
+            </div>
+
+            <div className="tile-container">
+
+            </div>
+          </div>
+        </div>
+
+  )
+    ;
   }
 }
 
